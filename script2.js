4095c552a693152d180b82b7efe62fb822bbaeec
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-05-08 10:34:44 +0100

    Replaced project with updated files, new game logic, new board construction, new player markers

--- script2.js
+++
@@ -1,8 +0,0 @@
-function loadGame() {
-    const playerX = new Player('x', 'player 1', 'x.svg')
-    const playerO = new Player('o', 'player 2', 'o.svg')
-    const game = new Game([playerX, playerO], 1)
-    game.startGame()
-}
-
-document.addEventListener('DOMContentLoaded', loadGame)
--- src/player.js
+++
@@ -1,8 +0,0 @@
-class Player {
-  // Player class represents the player in game
-  constructor(id, name, avatar = null) {
-    this.id = id;
-    this.name = name;
-    this.avatar = avatar;
-  }
-}
